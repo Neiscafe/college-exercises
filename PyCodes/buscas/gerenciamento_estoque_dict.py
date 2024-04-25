07499e2a4cd1604b526c3dff5e2006828c2ccee3
Guilherme Neis <guilhermeneis132@gmail.com>
2024-04-25 17:57:27 -0300

    Melhorias do gerenciamento de estoque

--- PyCodes/buscas/gerenciamento_estoque_dict.py
+++ PyCodes/buscas/gerenciamento_estoque_dict.py
@@ -7,8 +7,25 @@ acoes = {
     "Listar",5
     }
 
+def adicionar():
+    print("Digite a descrição e a quantidade:")
+
 print("1 - Adicionar produto")
 print("2 - Atualizar quantidade de produto")
 print("3 - Remover produto")
 print("4 - Consultar quantidade de produto")
 print("5 - Consultar todos os produtos")
+operacao = int(input("Digite a operação:"))
+
+if operacao==acoes["Adicionar"]:
+    adicionar()
+elif operacao==acoes["Atualizar quantidade"]:
+    atualizar_quantidade()
+elif operacao==acoes["Remover"]:
+    remover()
+elif operacao==acoes["Consultar"]:
+    consultar()
+elif operacao==acoes["Listar"]:
+    listar_produtos()
+else:
+    print("Tente novamente!")
