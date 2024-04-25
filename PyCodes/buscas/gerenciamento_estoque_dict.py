e62bfa8e98b85a234e9876ecc8dee4b5344e0932
Guilherme Neis <guilhermeneis132@gmail.com>
2024-04-25 17:17:28 -0300

    criação de base do arquivo de gerenciamento de estoque

---
+++ PyCodes/buscas/gerenciamento_estoque_dict.py
@@ -0,0 +1,14 @@
+estoque = dict()
+acoes = {
+    "Adicionar", 1, 
+    "Atualizar quantidade",2, 
+    "Remover",3, 
+    "Consultar",4, 
+    "Listar",5
+    }
+
+print("1 - Adicionar produto")
+print("2 - Atualizar quantidade de produto")
+print("3 - Remover produto")
+print("4 - Consultar quantidade de produto")
+print("5 - Consultar todos os produtos")
