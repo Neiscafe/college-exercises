4ad3f681ac439a7b662d1bb77a9f1c4cb850b1b6
Guilherme Neis <guilhermeneis132@gmail.com>
2024-06-26 09:38:23 -0300

    Correção: buffer da tela nos ultimos 10 segundos

--- PyCodes/produtividadee/timer.py
+++ PyCodes/produtividadee/timer.py
@@ -1,16 +1,18 @@
 import time
 import threading
 
+
 def my_start_countdown(tempo):
-    for min in reversed(range(0,tempo)):
-        for seg in reversed(range(0,60)):
+    for min in reversed(range(0, tempo)):
+        for seg in reversed(range(0, 60)):
             print(f"{min}:{seg}", end="\r", flush=True)
             time.sleep(1)
 
-while(True):
+
+while True:
     inputted_value = str(input("Esperando input:"))
     comandos = inputted_value.split()
-    if(comandos[0]!="timer"):
+    if comandos[0] != "timer":
         next
     tempo = int(comandos[1])
     tasks = comandos[2:]
@@ -20,8 +22,10 @@ while(True):
     #     print("\n")
     #     newInput = str(input(""))
     #     print(newInput)
-    for min in reversed(range(0,tempo)):
-        for seg in reversed(range(0,60)):
-            print(f"{min}:{seg}", end="\r", flush=True)
+
+    for minutes in reversed(range(0, tempo)):
+        for seg in reversed(range(0, 60)):
+            print("                                                     ", end="r")
+            print(f"{minutes}:{seg}", end="\r", flush=True)
             time.sleep(1)
     print("Time's up!")
