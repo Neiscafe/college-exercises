2b15da66d39704b4b5981177c2ad43a2aaa2bd1e
Guilherme Neis <guilhermeneis132@gmail.com>
2024-06-26 14:59:03 -0300

    Ideia de usar uma lista para o prompt

--- PyCodes/produtividadee/timer.py
+++ PyCodes/produtividadee/timer.py
@@ -3,11 +3,27 @@ import threading
 
 
 def my_start_countdown(tempo):
-    for min in reversed(range(0, tempo)):
+    for minutes in reversed(range(0, tempo)):
         for seg in reversed(range(0, 60)):
-            print(f"{min}:{seg}", end="\r", flush=True)
+            print(f"\x1b[1;2f{minutes}:{seg} ", end="\r", flush=True)
+            print("\x1b[3;2f")
             time.sleep(1)
+    print("Time's up!")
 
+
+# while True:
+#     print("\x1b[3;2f")
+#     inputted_value = str(input("\x1b[3;2fEsperando input:"))
+#     comandos = inputted_value.split()
+#     if comandos[0] != "timer":
+#         next
+#     tempo = int(comandos[1])
+#     tasks = comandos[2:]
+#     newThread = threading.Thread(target=my_start_countdown, args=[tempo])
+#     newThread.start()
+#     print("\n")
+#     newInput = str(input(""))
+#     print(newInput)
 
 while True:
     inputted_value = str(input("Esperando input:"))
@@ -15,17 +31,10 @@ while True:
     if comandos[0] != "timer":
         next
     tempo = int(comandos[1])
-    tasks = comandos[2:]
-    # newThread = threading.Thread(target=my_start_countdown, args=[tempo])
-    # newThread.start()
-    # while(newThread.is_alive):
-    #     print("\n")
-    #     newInput = str(input(""))
-    #     print(newInput)
-
     for minutes in reversed(range(0, tempo)):
         for seg in reversed(range(0, 60)):
-            print("                                                     ", end="r")
-            print(f"{minutes}:{seg}", end="\r", flush=True)
+            print(f"{minutes}:{seg} ", end="\r", flush=True)
             time.sleep(1)
     print("Time's up!")
+
+    
